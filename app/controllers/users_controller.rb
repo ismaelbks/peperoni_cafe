8ee29b0dcb0ce401bb31de07a4bab94101ae54cd
ismaelbks <ismael.boukhars@kedgebs.com>
2017-04-30 15:41:58 +0200

    espace-perso-design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 	def create
 	  user = User.new(user_params)
 	  if user.save
+	  	flash[:success] = "Bienvenue dans notre communauté !"
         UserMailer.welcome_email(user).deliver_later
 	    session[:user_id] = user.id
         redirect_to root_path, notice: 'User was successfully created.'
