ec01325f9be1253e6a62e7c7e864cd71f9f2a1e5
ismaelbks <ismael.boukhars@kedgebs.com>
2017-05-01 23:42:38 +0200

    user-models-testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ApplicationRecord
 
+	validates :name, presence: true, length: { maximum: 50 }
+
+	before_save { self.email = email.downcase }
+	validates :email, presence: true, length: { maximum: 255 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+ 	validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }, 
+                    uniqueness: true
+
+    before_save { self.pseudo = pseudo.downcase }
+    validates :pseudo, presence: false, length { maximum: 20 },
+    				uniqueness: true
+
 	has_secure_password
+	validates :password, length: { minimum: 6 }
 	
 end
